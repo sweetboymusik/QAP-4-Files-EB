5d969f8590776b00257701bfc885cf7188fe5392
Elliott Butt <ellbutt@gmail.com>
2023-11-21 23:34:39 -0330

    Project 3 updates.

---
+++ Project 3/PySample.py
@@ -0,0 +1,8 @@
+def getName():
+    name = input("What is your name? ")
+    return name
+
+
+RetailCost = 150.00
+
+print(f"The retail cost is {RetailCost},")
---
+++ Project 3/PythonDone/Stuff.py
@@ -0,0 +1,8 @@
+numbers = [1, 11, 66, 74, 879, 324, 12]
+
+sum = 0
+for num in numbers:
+    sum += num
+    print(num)
+
+print(sum)
